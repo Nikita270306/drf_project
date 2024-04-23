81b07040bfeaa238821944424822021993fcd7ed
nikita <n.shavkin@it-park.tech>
2024-04-23 11:07:48 +0300

    .

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,14 +1,30 @@
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from users.models import User
 from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.IntegerField(source='lesson_set.count', read_only=True)
+    lessons = serializers.SerializerMethodField()
+    owner = SlugRelatedField(slug_field='first_name', queryset=User.objects.all(), required=False)
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lessons(self, course):
+        return LessonListSerializer(Lesson.objects.filter(course=course), many=True).data
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class LessonListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ['id', 'title', 'description', 'preview', 'video_link']
--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -23,3 +25,19 @@ class User(AbstractUser):
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True)
+    payment_date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method_choices = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+    payment_method = models.CharField(max_length=10, choices=payment_method_choices)
+    stripe_id = models.CharField(max_length=300, verbose_name='stripe_id', blank=True, null=True)
+
+    def __str__(self):
+        return f"Payment: {self.user}, {self.payment_date}, {self.amount}"
